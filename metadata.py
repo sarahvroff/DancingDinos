61dcd4ed7221719db724a1bd42291feb40ecb0aa
Sarah Roff <94489722+sarahvroff@users.noreply.github.com>
2021-12-04 19:09:31 -0500

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -12,20 +12,17 @@ from copy import deepcopy
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+BaseImage = "ipfs://CIDCode"
+DancingDinos = ""
 
-# Base metadata. MUST BE EDITED.
-BASE_IMAGE_URL = "ipfs://<-- Your CID Code-->"
-BASE_NAME = ""
-
-BASE_JSON = {
-    "name": BASE_NAME,
+BaseJson = {
+    "name": DancingDinos,
     "description": "",
-    "image": BASE_IMAGE_URL,
+    "image": BaseImage,
     "attributes": [],
 }
 
 
-# Get metadata and JSON files path based on edition
 def generate_paths(edition_name):
     edition_path = os.path.join('output', 'edition ' + str(edition_name))
     metadata_path = os.path.join(edition_path, 'metadata.csv')
@@ -33,7 +30,6 @@ def generate_paths(edition_name):
 
     return edition_path, metadata_path, json_path
 
-# Function to convert snake case to sentence case
 def clean_attributes(attr_name):
     
     clean_name = attr_name.replace('_', ' ')
@@ -46,24 +42,18 @@ def clean_attributes(attr_name):
     clean_name = ''.join(clean_name)
     return clean_name
 
-
-# Function to get attribure metadata
 def get_attribute_metadata(metadata_path):
-
-    # Read attribute data from metadata file 
+ 
     df = pd.read_csv(metadata_path)
     df = df.drop('Unnamed: 0', axis = 1)
     df.columns = [clean_attributes(col) for col in df.columns]
 
-    # Get zfill count based on number of images generated
     zfill_count = len(str(df.shape[0]))
 
     return df, zfill_count
 
-# Main function that generates the JSON metadata
 def main():
 
-    # Get edition name
     print("Enter edition you want to generate metadata for: ")
     while True:
         edition_name = input()
@@ -76,38 +66,30 @@ def main():
             print("Oops! Looks like this edition doesn't exist! Check your output folder to see what editions exist.")
             print("Enter edition you want to generate metadata for: ")
             continue
-    
-    # Make json folder
+ 
     if not os.path.exists(json_path):
         os.makedirs(json_path)
-    
-    # Get attribute data and zfill count
+
     df, zfill_count = get_attribute_metadata(metadata_path)
     
     for idx, row in progressbar(df.iterrows()):    
-    
-        # Get a copy of the base JSON (python dict)
+
         item_json = deepcopy(BASE_JSON)
-        
-        # Append number to base name
+ 
         item_json['name'] = item_json['name'] + str(idx)
 
         # Append image PNG file name to base image path
         item_json['image'] = item_json['image'] + '/' + str(idx).zfill(zfill_count) + '.png'
-        
-        # Convert pandas series to dictionary
+
         attr_dict = dict(row)
-        
-        # Add all existing traits to attributes dictionary
+
         for attr in attr_dict:
             
             if attr_dict[attr] != 'none':
                 item_json['attributes'].append({ 'trait_type': attr, 'value': attr_dict[attr] })
-        
-        # Write file to json folder
+
         item_json_path = os.path.join(json_path, str(idx))
         with open(item_json_path, 'w') as f:
             json.dump(item_json, f)
 
-# Run the main function
 main()
